150f8b3bef00789f97960029df44ab871e523e35
Lucas Monteiro <lucasmsantos.ti@gmail.com>
2024-10-08 20:57:11 -0300

    change of data.jason

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,10 @@
+let quiz = {};
+let pontos = 0;
+let pergunta = 1;
+
+function buscar_perguntas () {
+    const url_dados = "../../data.json";
+    fetch(url_dados).then(resposta => resposta.json()).then(dados => {
+
+    })
+};
